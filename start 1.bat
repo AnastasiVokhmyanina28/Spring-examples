0a8a68794265a9c327ad82d38b510a65d1ee9303
Анастасия Вохмянина <55682455+AnastasiVokhmyanina28@users.noreply.github.com>
2022-04-29 14:16:51 +0300

    task1-2

--- start 1.bat
+++ start 1.bat
@@ -2,6 +2,8 @@ javac -d src/main/classes -classpath "src/main/resources/libs/*"  -sourcepath sr
 javac -d src/main/classes -classpath "src/main/resources/libs/*"  -sourcepath src/main/java "src/main/java/org/example/K2/*.java"
 javac -d src/main/classes -classpath "src/main/resources/libs/*"  -sourcepath src/main/java "src/main/java/org/example/K3/*.java"
 javac -d src/main/classes -classpath "src/main/resources/libs/*"  -sourcepath src/main/java "src/main/java/org/example/K4/*.java"
+javac -d src/main/classes -classpath "src/main/resources/libs/*"  -sourcepath src/main/java "src/main/java/org/example/zadanie/*.java"
+javac -d src/main/classes -classpath "src/main/resources/libs/*"  -sourcepath src/main/java "src/main/java/org/example/zadanie2/*.java"
 
 
 xcopy  /s /i /A src\main\resources\ src\main\classes
@@ -9,5 +11,7 @@ java -classpath "src/main/classes;src/main/resources/libs/*" org.example.K1.Knig
 java -classpath "src/main/classes;src/main/resources/libs/*" org.example.K2.Idol
 java -classpath "src/main/classes;src/main/resources/libs/*" org.example.K3.MagicMain
 java -classpath "src/main/classes;src/main/resources/libs/*" org.example.K4.MagicMain4
+java -classpath "src/main/classes;src/main/resources/libs/*" org.example.zadanie.Main
+java -classpath "src/main/classes;src/main/resources/libs/*" org.example.zadanie2.MainAB
 
 pause
